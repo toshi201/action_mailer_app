ae4ce2381db941693bd1b4ce38febf8ac6fdde3c
toshi201 <aquaconner8@gmail.com>
2022-09-23 09:03:21 +0900

    user mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,6 @@
+class UserMailer < ApplicationMailer
+  def welcome
+    @name = params[:name]
+    mail(to: params[:to], subject: '登録完了')
+  end
+end
---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,6 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+  def welcome
+    UserMailer.with(to: "igarash@example", name: "igaiga").welcome
+  end
+end
